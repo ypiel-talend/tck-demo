9ad2a5888543995cef8cc64f93d220c0c5158bf8
Yves Piel <ypiel@talend.com>
2024-12-18 08:58:35 +0100

    Manager versions in migration handler.

--- src/main/java/org/Talend/demo/tckdemo/migration/DSOMigration.java
+++ src/main/java/org/Talend/demo/tckdemo/migration/DSOMigration.java
@@ -1,6 +1,8 @@
 package org.Talend.demo.tckdemo.migration;
 
+import org.Talend.demo.tckdemo.config.Datastore;
 import org.talend.sdk.component.api.component.MigrationHandler;
+import org.talend.sdk.component.api.configuration.type.DataStore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +11,9 @@ public class DSOMigration implements MigrationHandler {
     public Map<String, String> migrate(int incomingVersion, Map<String, String> incomingData) {
         Map<String, String> duplicate = new HashMap<String, String>(incomingData);
 
-        duplicate.putIfAbsent("configuration.dataset.datastore.port", "3310");
+        if(incomingVersion < Datastore.VERSION) {
+            duplicate.putIfAbsent("configuration.dataset.datastore.port", "3310");
+        }
 
         return duplicate;
     }
